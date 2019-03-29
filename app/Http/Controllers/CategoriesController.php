c78dc68660b83578a20b69a8ec560c8cdcee7ec5
tatsurodev <contacts@tatsuro.dev>
2019-03-29 22:51:55 +0900

    CategoriesControllerのindexメソッドとそのビューを作成(25)

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -16,7 +16,7 @@ class CategoriesController extends Controller
      */
     public function index()
     {
-        return view('categories.index');
+        return view('categories.index')->with('categories', Category::all());
     }
 
     /**
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -7,5 +7,19 @@
     <div class="card-header">
         Categories
     </div>
+    <div class="card-body">
+        <table class="table">
+            <thead>
+                <th>Name</th>
+            </thead>
+            <tbody>
+                @foreach ($categories as $category)
+                <tr>
+                    <td>{{ $category->name }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 @endsection
