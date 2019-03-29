fdad662146a9be970d8932033eb27fdff2460335
tatsurodev <contacts@tatsuro.dev>
2019-03-30 02:35:49 +0900

    create.blade.phpの表示ををcreate,edit時にそれぞれ変更

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="card card-default">
     <div class="card-header">
-        Create Category
+        {{ isset($category) ? 'Edit category' : 'Create category' }}
     </div>
     <div class="card-body">
         @if ($errors->any())
@@ -16,14 +16,19 @@
             </ul>
         </div>
         @endif
-        <form action="{{ route('categories.store') }}" method="post">
+        <form action="{{ isset($category) ? route('categories.update', $category->id) : route('categories.store') }}"
+            method="POST">
             @csrf
+            @if (isset($category))
+            @method('PUT')
+            @endif
             <div class="form-group">
                 <label for="name">Name</label>
-                <input type="text" class="form-control" name="name" id="name">
+                <input type="text" class="form-control" name="name" id="name"
+                    value="{{ isset($category) ? $category->name : '' }}">
             </div>
             <div class="form-group">
-                <button class="btn btn-success">Add Category</button>
+                <button class="btn btn-success">{{ isset($category) ? 'Update category' : 'Add category' }}</button>
             </div>
         </form>
     </div>
