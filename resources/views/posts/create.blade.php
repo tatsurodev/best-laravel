7288a2839f04aa1d830dc2a80a3644a4eee4611b
tatsurodev <contacts@tatsuro.dev>
2019-04-03 14:39:55 +0900

    trix editorをcreate.blade.phpにインストール

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -15,7 +15,8 @@
             </div>
             <div class="form-group">
                 <label for="content">Content</label>
-                <textarea name="content" id="content" cols="5" rows="5" class="form-control"></textarea>
+                <input id="content" type="hidden" name="content">
+                <trix-editor input="content"></trix-editor>
             </div>
             <div class="form-group">
                 <label for="published_at">Published At</label>
@@ -33,4 +34,12 @@
         </form>
     </div>
 </div>
+@endsection
+
+@section('script')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.1.0/trix.js"></script>
+@endsection
+
+@section('css')
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.1.0/trix.css">
 @endsection
