67b8d480e079a41f4d56f0f378a1550445af4ff2
tatsurodev <contacts@tatsuro.dev>
2019-04-07 05:19:11 +0900

    ユーザー認証が必須な部分をauthミドルウェアでまとめて登録(40)

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,14 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('categories', 'CategoriesController');
+    Route::resource('categories', 'CategoriesController');
 
-Route::resource('posts', 'PostsController');
+    Route::resource('posts', 'PostsController');
 
-Route::get('trashed-posts', 'PostsController@trashed')->name('trashed-posts.index');
+    Route::get('trashed-posts', 'PostsController@trashed')->name('trashed-posts.index');
 
-Route::put('restore-post/{post}', 'PostsController@restore')->name('restore-posts');
+    Route::put('restore-post/{post}', 'PostsController@restore')->name('restore-posts');
+});
