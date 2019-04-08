67ed5a46864da005333e595bcfd96ba5c4075f92
tatsurodev <contacts@tatsuro.dev>
2019-04-08 18:33:39 +0900

    usersテーブルに管理者かただのライターかを表すフィールドを追加、管理者データをseedに用意

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'writer'])->default('writer');
+            $table->text('about')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
