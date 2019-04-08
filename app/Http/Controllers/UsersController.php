038a9cb5b570528eabcb952f76c658d2c78a44a0
tatsurodev <contacts@tatsuro.dev>
2019-04-08 22:31:06 +0900

    ユーザープロフィールの更新機能を作成(UsersControllerのupdateアクション、カスタムリクエスト、ブレード、ルーターを作成, 47)

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\User;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\Users\UpdateProfileRequest;
 
 class UsersController extends Controller
 {
@@ -17,6 +18,17 @@ class UsersController extends Controller
     {
         // routeから必須パラメーターを貰ってユーザーを取得するとgetで他のユーザープロフィールも編集できることになり、ユーザーが自分のプロフィールのみ編集可にするミドルウェアが必要になり手間がかかるので直接現在のユーザーインスタンスを渡す
         return view('users.edit')->with('user', auth()->user());
+    }
+
+    public function update(UpdateProfileRequest $request)
+    {
+        $user = auth()->user();
+        $user->update([
+            'name' => $request->name,
+            'about' => $request->about
+        ]);
+        session()->flash('success', 'User updated successfully.');
+        return redirect()->back();
     }
 
     public function makeAdmin(User $user)
