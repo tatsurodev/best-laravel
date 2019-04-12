43b70193de4d36a3681f825db97bf869111d55bc
tatsurodev <contacts@tatsuro.dev>
2019-04-12 21:01:39 +0900

    DiscussionsController@show作成し、モデルバインディングで使用するキーをslugに変更

--- app/Discussion.php
+++ app/Discussion.php
@@ -4,5 +4,10 @@ namespace LaravelForum;
 
 class Discussion extends Model
 {
-    //
+
+    // ルートモデルバインディング使用時のprimary keyをslugに変更
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
