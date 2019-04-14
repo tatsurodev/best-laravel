7a1506f5de86457d7fb4a5625e3ae4e7fb1188ef
tatsurodev <contacts@tatsuro.dev>
2019-04-14 21:12:19 +0900

    ログイン後のリダイレクトをdiscussionsに変更

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,8 +17,9 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        // ログイン後にlogin, registerにアクセスがあるとここにリダイレクト
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return redirect('/discussions');
         }
 
         return $next($request);
