7e6ebd9e7919708b5f8cd475ae89de06ba757bc2
tatsurodev <contacts@tatsuro.dev>
2019-04-15 22:49:07 +0900

    未読通知を既読にし表示するUsersController@notificationsとそのビューを作成(64)

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,7 +8,12 @@ class UsersController extends Controller
 {
     public function notifications()
     {
+        // dd(auth()->user()->notifications->first()->data['discussion']['slug']);
         // mark all as read
+        auth()->user()->unreadNotifications->markAsRead();
         // display all notifications
+        return view('users.notifications', [
+            'notifications' => auth()->user()->notifications()->paginate(5),
+        ]);
     }
 }
