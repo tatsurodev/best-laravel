806a83cbdc80d9be2d14d6b3f6201862a609de3d
Tatsuro <contacts@tatsuro.dev>
2019-11-30 02:24:53 +0900

    checkout後のmail機能作成
    art make:mail PurchaseSuccessful

--- udemy-course-ecommerce/app/Http/Controllers/CheckoutController.php
+++ udemy-course-ecommerce/app/Http/Controllers/CheckoutController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\PurchaseSuccessful;
 use Cart;
+use Mail;
 use Stripe\Charge;
 use Stripe\Stripe;
 use Illuminate\Http\Request;
@@ -27,5 +29,11 @@ class CheckoutController extends Controller
             // request()でstripから返ってきたrequestが取得でき、stripeToken propertyにtokenが格納されている
             'source' => request()->stripeToken,
         ]);
+        // cart中身削除
+        Cart::destroy();
+
+        // mail送信
+        Mail::to(request()->stripeEmail)->send(new PurchaseSuccessful);
+        return redirect()->route('index')->withSuccess('Purchase successfull. Wait for our email.');
     }
 }
---
+++ udemy-course-ecommerce/resources/views/emails/purchase.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Purchase successfull</title>
+</head>
+<body>
+    <h1>Thank you for puchasing</h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum perferendis tenetur, id qui veritatis natus possimus? Modi perferendis cum distinctio amet maiores mollitia debitis veniam et aperiam aliquid, ea nobis!
+    </p>
+</body>
+</html>
