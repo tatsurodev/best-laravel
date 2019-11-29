a27a79a0fbaf8921c2974992fb5f669ab6c68a7d
Tatsuro <contacts@tatsuro.dev>
2019-11-29 15:57:13 +0900

    ecommerce用template install 181

--- udemy-course-ecommerce/routes/web.php
+++ udemy-course-ecommerce/routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'FrontEndController@index')->name('index');
 
 Route::resource('products', 'ProductController');
 
